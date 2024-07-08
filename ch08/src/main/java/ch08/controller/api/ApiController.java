ffd8fe7781666395d0e968f33ffd947443043f4b
born-A <93516595+born-A@users.noreply.github.com>
2024-07-08 17:03:49 +0900

    ajax

---
+++ ch08/src/main/java/ch08/controller/api/ApiController.java
@@ -0,0 +1,23 @@
+package ch08.controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class ApiController {
+	
+	@ResponseBody
+	@RequestMapping("/text")
+	public String text() {
+		return "Text 데이터";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/html")
+	public String html() {
+		return "html";
+	}
+	
+}
